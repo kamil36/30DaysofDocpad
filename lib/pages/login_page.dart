ea26284848d3aa4f3c7ab785f90a2ef21638c5de
Kamil#098 <kamilbombe855@gmail.com>
2022-11-04 19:36:44 +0530

    Day 4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,17 +4,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-          textScaleFactor: 1.5,
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              height: 350,
+              width: 350,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter a Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter a Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Docpad");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
