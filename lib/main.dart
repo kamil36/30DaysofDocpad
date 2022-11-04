db3d7b9c5a92803e3a3034b5bf967d18623090aa
Kamil#098 <kamilbombe855@gmail.com>
2022-11-04 11:40:06 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:docpad/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+import 'pages/home_page.dart';
+
+void main() {
   runApp(MyApp());
 }
 
@@ -10,13 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Docpad App"),
-          ),
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
