2cd3eb3c899085c992bc51276d506dce19ea59de
Kamil#098 <kamilbombe855@gmail.com>
2022-11-26 19:33:09 +0530

    Day 17 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:docpad/pages/login_page.dart';
 import 'package:docpad/utils/routes.dart';
 import 'package:docpad/utils/widgets/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:docpad/utils/widgets/themes.dart';
+
 import 'pages/home_page.dart';
 
 void main() {
